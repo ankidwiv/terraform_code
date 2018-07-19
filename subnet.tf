677cfd5c5604a2ce6c0213de4b2d4b4d5f08dae2
root <root@oc2556863456.ibm.com>
2018-07-20 01:51:33 +0530

    trying using list of AZ

--- subnet.tf
+++ subnet.tf
@@ -1,8 +1,8 @@
 resource "aws_subnet" "Public_subnet" {
   vpc_id                  = "${aws_vpc.My_Vpc.id}"
-  cidr_block              = "${element(var.cidr_blocks, count.index )}"
+  cidr_block              = "${element(var.cidr_blocks, 0)}"
   map_public_ip_on_launch = 1
-  availability_zone       = "${lookup(var.zones, concat("zone", count.index))}"
+  availability_zone       = "${element(lookup(var.AZ, var.aws_region), 0)}"
 
   tags {
     name = " Public_subnet "
