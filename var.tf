8c2aa74843dbf6e93202bb98ef7eee922528b8e8
Cloud User <ec2-user@ip-172-31-20-219.ap-south-1.compute.internal>
2018-07-20 02:14:42 +0530

    formating the terraform file

--- var.tf
+++ var.tf
@@ -1,8 +1,8 @@
 variable "aws_region" {
   default = {
-  ap-south-1 = "ap-south-1"
-  us-east-1 = "us-east-1a"
- 
+    ap-south-1 = "ap-south-1"
+    us-east-1  = "us-east-1a"
+  }
 }
 
 variable "zones" {
@@ -17,47 +17,40 @@ variable "zones" {
 variable "cidr_blocks" {
   type = "list"
 
-  default = ["10.0.1.0/24","10.0.2.0/24"]
-  
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "AZ" {
-    type = "map"
-    default = {
+  type = "map"
+
+  default = {
     ap-south-1 = ["ap-south-1a", "ap-south-1b"]
-    us-east-1 = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d" ]
-    }
+    us-east-1  = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
+  }
 }
 
 variable "instance_count" {
-
-    default = 2
+  default = 2
 }
 
 variable "ami" {
-    type = "map"
-    
-    default = {
-    us-east-1 = "ami-6871a115"
+  type = "map"
+
+  default = {
+    us-east-1  = "ami-6871a115"
     ap-south-1 = "ami-5b673c34"
-    
-    }
-    
+  }
 }
 
 variable "instance_type" {
-    default = "t2.micro"
-    
+  default = "t2.micro"
 }
-
 
 variable "ssh_key" {
-    type = "map"
-    default = {
-    us-east-1 = "My-us-east-key"
+  type = "map"
+
+  default = {
+    us-east-1  = "My-us-east-key"
     ap-south-1 = "MyEc2key"
-    
-    }
-    
+  }
 }
-    
