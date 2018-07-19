8ba5f03d3b7e134e61fa13b4dc54badd593bc663
Cloud User <ec2-user@ip-172-31-20-219.ap-south-1.compute.internal>
2018-07-19 23:33:38 +0530

    correcting instance.tf

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my-instances" {
     count = "${var.instance_count}"
     ami = "${lookup(var.ami, var.aws_region)}"
     instance_type = "${var.instance_type}"
-    key_name = "${lookup(var.ssh_key, var.aws_region}"
+    key_name = "${lookup(var.ssh_key, var.aws_region)}"
     tags {
         Name = "Terraform-${count.index}"
         }
