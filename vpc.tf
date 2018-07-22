d0793ab80bf799c4ed1ffaaea301078ee49f18e1
Cloud User <ec2-user@ip-172-31-20-219.ap-south-1.compute.internal>
2018-07-22 15:24:51 +0530

    validated code rt, eip,subnet,vpc,IGW,instances

--- vpc.tf
+++ vpc.tf
@@ -9,6 +9,15 @@ resource "aws_vpc" "My_Vpc" {
   }
 }
 
+resource "aws_internet_gateway" "My_IGW"{
+
+	vpc_id = "${aws_vpc.My_Vpc.id}"
+	tags {
+	Name = "My_IGW"
+}
+}
+
+
 
 #Creating Public subnets
 
@@ -24,5 +33,16 @@ resource "aws_subnet" "Public_subnet" {
   }
 }
 
+#Creating private subnets
+resource "aws_subnet" "Private_subnet" {
+    count = "${length(var.Private_subnet_cidrs)}"
+  vpc_id                  = "${aws_vpc.My_Vpc.id}"
+  cidr_block              = "${element(var.Private_subnet_cidrs, count.index )}"
+  map_public_ip_on_launch = 1
+  availability_zone       = "${element(var.AZ, count.index)}"
 
+  tags {
+    name = "Private_subnet-${count.index +1}"
+  }
+}
 
