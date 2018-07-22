3fe9da13772481213c8519f640ef70e1578b1e9a
Cloud User <ec2-user@ip-172-31-20-219.ap-south-1.compute.internal>
2018-07-22 15:37:51 +0530

    validated file merging vpc and gateways

--- subnets.tf
+++ subnets.tf
@@ -1,24 +1,3 @@
-#Creating VPC 
-resource "aws_vpc" "My_Vpc" {
-  cidr_block           = "${var.vpc_cidr}"
-  enable_dns_support   = 1
-  enable_dns_hostnames = 1
-
-  tags {
-    name = "My_Vpc"
-  }
-}
-
-resource "aws_internet_gateway" "My_IGW"{
-
-	vpc_id = "${aws_vpc.My_Vpc.id}"
-	tags {
-	Name = "My_IGW"
-}
-}
-
-
-
 #Creating Public subnets
 
 resource "aws_subnet" "Public_subnet" {
