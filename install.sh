7efc160d28c9be52e87067940c6a321348dec2ee
ankit dwivedi <ankit@oc2556863456.ibm.com>
2018-07-23 21:10:01 +0530

    correcting install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 Install_packages()
 {
-    sudo yum install httpd php* wget mariadb -y
+     yum install httpd php* wget mariadb -y
  }
 starting_service()
 {
-     sudo systemctl start httpd
+      systemctl start httpd
  }
 nfs_conf()
 {
@@ -15,17 +15,17 @@ nfs_conf()
 }
 Drupal_deploy()
 {
-     sudo wget https://ftp.drupal.org/files/projects/drupal-7.53.tar.gz
-     sudo tar -xvzf drupal-7.53.tar.gz
-     sudo cp -R drupal-7.53/* /var/www/html
-     sudo mkdir /var/www/html/sites/default/files
-     sudo cp /var/www/html/sites/default/default.settings.php /var/www/html/sites/default/settings.php
-     sudo chown -R apache:apache /var/www/
+      wget https://ftp.drupal.org/files/projects/drupal-7.53.tar.gz
+      tar -xvzf drupal-7.53.tar.gz
+      cp -R drupal-7.53/* /var/www/html
+      mkdir /var/www/html/sites/default/files
+      cp /var/www/html/sites/default/default.settings.php /var/www/html/sites/default/settings.php
+      chown -R apache:apache /var/www/
 }
 restart_service()
 {
-     sudo systemctl reload httpd
-     sudo systemctl enable httpd
+      systemctl reload httpd
+      systemctl enable httpd
 }
 Install_packages
 starting_service
