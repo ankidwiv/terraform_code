916a0099c9cb1fe5bc46f3c1eb4d9b03ae90967a
Cloud User <ec2-user@ip-172-31-20-219.ap-south-1.compute.internal>
2018-07-23 21:51:47 +0530

    adding efs and output.tf

---
+++ lb.tf
@@ -0,0 +1,20 @@
+resource "aws_lb" "AppELB" {
+  name               = "AppELB"
+  security_groups    = ["${aws_security_group.LB_SG.id}"]
+  subnets            = ["${aws_subnet.Public_subnet.*.id}"]
+  internal           = false
+  load_balancer_type = "application"
+}
+
+resource "aws_lb_target_group" "AppTG" {
+  name     = "ALBTG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${aws_vpc.My_Vpc.id}"
+}
+
+resource "aws_lb_target_group_attachment" "AppTGattach" {
+  target_group_arn = "${aws_lb_target_group.AppTG.arn}"
+  target_id        = "${element(aws_instance.my-instances.*.id, var.instance_count)}"
+  port             = 80
+}
