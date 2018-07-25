2e23cd70ca1ff499d78117a3f5e7c2e59a714a24
Cloud User <ec2-user@ip-172-31-20-219.ap-south-1.compute.internal>
2018-07-26 02:16:28 +0530

    working code V1

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ Aws_cli()
  aws configure set default.region $my_region
  aws efs describe-file-systems --region $my_region
  fs_id=`aws efs describe-file-systems --region us-east-1| grep fs- |awk '{print $2}'|tr -d \'\"\,`
- Mount_target="$fs_id.nfs.$my_region.$my_domain"
+ Mount_target="$fs_id.efs.$my_region.$my_domain"
  Mount_point="/mnt/efs"
 # mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $Mount_target:/ /mnt/efs
  echo "$Mount_target:/ $Mount_point nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 0 0"| tee --append /etc/fstab
