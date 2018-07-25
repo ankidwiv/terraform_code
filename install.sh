d6f7de7ab768a14bb54394d5f37c9e786ae2067c
Cloud User <ec2-user@ip-172-31-20-219.ap-south-1.compute.internal>
2018-07-26 02:11:55 +0530

    running userdata to mount efs

--- install.sh
+++ install.sh
@@ -17,14 +17,18 @@ Aws_cli()
  aws configure set default.region $my_region
  aws efs describe-file-systems --region $my_region
  fs_id=`aws efs describe-file-systems --region us-east-1| grep fs- |awk '{print $2}'|tr -d \'\"\,`
- fs_name="$fs_id.nfs.$my_region.$my_domain"
- Mount_target="$fs_id.$my_region.$my_domain"
- mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $Mount_target:/ /mnt/efs
+ Mount_target="$fs_id.nfs.$my_region.$my_domain"
+ Mount_point="/mnt/efs"
+# mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $Mount_target:/ /mnt/efs
+ echo "$Mount_target:/ $Mount_point nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 0 0"| tee --append /etc/fstab
  mount -a
 }
 starting_service()
 {
       systemctl start httpd
+      systemctl enable httpd
+      systemctl start nfs
+      systemctl enable nfs
  }
 
 Drupal_deploy()
